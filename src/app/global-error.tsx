5412738e1fbcfbba1d6c25a416055a76106a4838
Jerry-Tran-GoldenOwl <jerry@a26k.ch>
2025-10-29 16:45:35 +0700

    feat: setup sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,15 +1,19 @@
-'use client';
+"use client";
 
-import ErrorPage from '@/components/sections/ErrorPage';
-import { useEffect } from 'react';
+import ErrorPage from "@/components/sections/ErrorPage";
+import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
 
 export default function GlobalError(props: {
   error: Error & { digest?: string };
   params: { locale: string };
 }) {
   useEffect(() => {
-    // Handle side effects related to the error
-    console.error(props.error);
+    Sentry.captureException(props.error);
+
+    if (process.env.NODE_ENV === "development") {
+      console.error(props.error);
+    }
   }, [props.error]);
 
   return <ErrorPage errorCode={500} />;
